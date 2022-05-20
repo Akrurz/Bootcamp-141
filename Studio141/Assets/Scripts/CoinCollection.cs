8220cf4a0e14057d08a60506cfd0495f99edba66
sabuhakaya <sabuha.kaya435@gmail.com>
2022-05-20 16:58:13 +0300

    Collectables upgraded

--- Studio141/Assets/Scripts/CoinCollection.cs
+++ Studio141/Assets/Scripts/CoinCollection.cs
@@ -2,44 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MusicListNS;
-using TMPro;
-
 using StarterAssets;
-public class CoinCollection : MonoBehaviour
-{
-    // Start is called before the first frame update
-    GameObject music;
-    private CoinInst randomizer;
-    [SerializeField] GameObject _gameObject;
-   // private int totalSpawnPoints;
-    int y;
-    ThirdPersonController thirdPersonController;
-    //[SerializeField] private TMP_Text _text;
-    private int count= 0;
+using CDTimerNS; //Countdown timer
 
-    MusicList musicList;
-    private void Start()
-    {
-        music = GameObject.Find("AudioManager");
-        musicList = music.GetComponent(typeof(MusicList)) as MusicList;
-        thirdPersonController = GetComponent<ThirdPersonController>();
-     
 
-    }
-    private void OnTriggerEnter(Collider other)
+    public class CoinCollection : MonoBehaviour
     {
-        if (other.gameObject.CompareTag("food"))
+       
+        // Start is called before the first frame update
+        GameObject music;
+        private CoinInst randomizer;
+        [SerializeField] GameObject _gameObject;
+        // private int totalSpawnPoints;
+        int y;
+        ThirdPersonController thirdPersonController;
+
+        private int count = 0;
+
+        MusicList musicList;
+      
+        private void Start()
         {
-            count++;
-            other.gameObject.SetActive(false);
-            AudioSource.PlayClipAtPoint(musicList.music[0], gameObject.transform.position);
-            StartCoroutine(Spawn(other.gameObject));
+            music = GameObject.Find("AudioManager");
+            musicList = music.GetComponent(typeof(MusicList)) as MusicList;
+        
+            thirdPersonController = GetComponent<ThirdPersonController>();
+
+
         }
-    }
-    IEnumerator Spawn(GameObject _gameObject){
-        yield return new WaitForSeconds(3);
-        gameObject.SetActive(true);
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("food"))
+            {
+                count++;
+             CDTimer.currentTime= CDTimer.currentTime+30;
+                other.gameObject.SetActive(false);
+                AudioSource.PlayClipAtPoint(musicList.music[0], gameObject.transform.position);
+                StartCoroutine(Spawn(other.gameObject));
+            }
+        }
+        IEnumerator Spawn(GameObject _gameObject)
+        {
+            yield return new WaitForSeconds(3);
+            gameObject.SetActive(true);
+
+        }
 
     }
 
-}
